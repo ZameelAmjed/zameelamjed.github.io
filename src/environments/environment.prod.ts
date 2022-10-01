2a1489e6f7546070ee2d3b7e3dea33343801461c
Zameel Amjed <zameelamjed@yahoo.com>
2022-10-01 11:26:50 +0530

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name:'Zameel Amjed',
   apiExperiance:'https://api.npoint.io/90501af4c939d4b5d3ff',
-  apiProfile: '/assets/profile.json'
+  apiProfile: '/assets/content/profile.json'
 };
