dfd6696a08b14bc64cbf331e12dd014c12d37c7c
Zameel Amjed <zameelamjed@yahoo.com>
2022-10-01 21:06:06 +0530

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  name:'Zameel Amjed',
-  apiExperiance:'https://api.npoint.io/90501af4c939d4b5d3ff',
-  apiProfile: 'https://api.npoint.io/7fdc72e5033a70951cc0'
-};
